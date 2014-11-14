31256f0f3955531ca9123cd5006d5d5a008cd163
tscully49 <thomas1374@yahoo.com>
2014-11-13 18:20:37 -0600

    Set up node server with PG and everything installed

---
+++ app.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// THIS PART IS A TEST ////////////////////////////////////////////////////////
+/*var http = require('http');
+var pg = require('pg');
+
+var conString = "postgres://postgres:1234@localhost/postgres";
+
+var server = http.createServer(function(req, res) {
+
+  // get a pg client from the connection pool
+  pg.connect(conString, function(err, client, done) {
+
+    var handleError = function(err) {
+      // no error occurred, continue with the request
+      if(!err) return false;
+
+      // An error occurred, remove the client from the connection pool.
+      // A truthy value passed to done will remove the connection from the pool
+      // instead of simply returning it to be reused.
+      // In this case, if we have successfully received a client (truthy)
+      // then it will be removed from the pool.
+      done(client);
+      res.writeHead(500, {'content-type': 'text/plain'});
+      res.end('An error occurred');
+      return true;
+    };
+
+    // record the visit
+    client.query('INSERT INTO visit (date) VALUES ($1)', [new Date()], function(err, result) {
+
+      // handle an error from the query
+      if(handleError(err)) return;
+
+      // get the total number of visits today (including the current visit)
+      client.query('SELECT COUNT(date) AS count FROM visit', function(err, result) {
+
+        // handle an error from the query
+        if(handleError(err)) return;
+
+        // return the client to the connection pool for other requests to reuse
+        done();
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.end('You are visitor number ' + result.rows[0].count);
+      });
+    });
+  });
+})
+
+server.listen(3001)*/
+// ENDING THE TEST PART ///////////////////////////////////////////////////////
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
